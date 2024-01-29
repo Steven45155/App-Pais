b41d9775c90472dc354dd352303ed6845ae64d4b
Steven_Suarez <stiven451556@gmail.com>
2024-01-28 19:25:54 -0500

    ajuste de url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.components';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.components';
+import { AppComponent } from './app.component';
 
 const routes:Routes=[
-
   {
-    path: 'about',
-    component: AboutPageComponent
-  },
-  {
-    path: 'contact',
-    component: ContactPageComponent
-  },
-  {
-    path: 'countries',
+    path: '',
     loadChildren: ()=> import('./countries/countries.module').then(m =>m.CountriesModule)
   },
   {
     path: '**',
-    redirectTo: 'countries'
-  },
-
+    redirectTo: ''
+  }
 ]
 
 @NgModule({
